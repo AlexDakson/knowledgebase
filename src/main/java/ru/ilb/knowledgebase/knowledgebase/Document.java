c2aea3618204d10fe5b347b36a2ae08711983b7d
AlexDakson <cordozoalex@gmail.com>
2020-03-03 15:18:40 +0400

    Добавлены классы

---
+++ src/main/java/ru/ilb/knowledgebase/knowledgebase/Document.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.ilb.knowledgebase.knowledgebase;
+
+import java.util.List;
+/**
+ *
+ * @author develop01
+ */
+public class Document {
+    private int id;
+    private String source;
+    private List<Resource> resources;
+}
---
+++ src/main/java/ru/ilb/knowledgebase/knowledgebase/Podpiska.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.ilb.knowledgebase.knowledgebase;
+
+/**
+ *
+ * @author develop01
+ */
+public class Podpiska {
+    private User user;
+    private Resource resource;
+}
